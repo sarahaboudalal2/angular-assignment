1a6b6bc3ad31842c508b30aa7a1160620033745e
sarahaboudalal <sarahaboudalal@gmail.com>
2023-03-29 16:38:43 +0300

    added cards using Input decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-assignment-1';
+export class AppComponent implements OnInit {
+  public productsInfo: any;
+  public constructor(private http: HttpClient) {}
+  public ngOnInit(): void {
+    const url: string = './assets/products.json';
+    this.http.get(url).subscribe((response) => {
+      this.productsInfo = response;
+      console.log(this.productsInfo);
+    });
+  }
+
+  getProductsInfp() {
+    return this.productsInfo;
+    console.log(this.productsInfo);
+  }
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+import { Component, Input } from '@angular/core';
 import { CardClickService } from '../services/cardClick.service';
 
 export type Product = {
@@ -14,23 +14,29 @@ export type Product = {
   styleUrls: ['./products.component.css'],
   providers: [CardClickService],
 })
-export class ProductsComponent implements OnInit {
-  public productsInfo: any;
-  public constructor(
-    private http: HttpClient,
-    private cardClick: CardClickService
-  ) {}
-  public ngOnInit(): void {
-    const url: string = './assets/products.json';
-    this.http.get(url).subscribe((response) => {
-      this.productsInfo = response;
-    });
-  }
+export class ProductsComponent {
+  @Input() products = [
+    {
+      id: 0,
+      product: '',
+      price: 0,
+    },
+  ];
+  // public constructor(
+  //   private cardClick: CardClickService
+  // ) {}
+  // public ngOnInit(): void {
+  //   const url: string = './assets/products.json';
+  //   this.http.get(url).subscribe((response) => {
+  //     this.productsInfo = response;
+  //   });
+  // }
   // onClick(product: Product): void {
   //   console.log(product.product);
   // }
-  onClick(product: Product) {
-    // const cardClick = new CardClickService();
-    this.cardClick.OnCardClick(product);
-  }
+
+  // onClick(product: Product) {
+  //   // const cardClick = new CardClickService();
+  //   this.cardClick.OnCardClick(product);
+  // }
 }
