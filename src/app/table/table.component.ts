99fbd097b64433f7c76a5ed4cd6eb4cf4441cbce
sarahaboudalal <sarahaboudalal@gmail.com>
2023-04-04 22:39:42 +0300

    Implemented calculation of the total price and displaying it in total section

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Product } from '../products/products.component';
 
 @Component({
   selector: 'app-table',
@@ -14,4 +15,19 @@ export class TableComponent {
       quantity: 0,
     },
   ];
+
+  getPriceTimesQuantity(product: Product) {
+    const multiply: number = parseInt(
+      (product.price * product.quantity).toFixed(2)
+    );
+    return multiply;
+  }
+
+  getTotalPrice() {
+    let totalPrice: number = 0;
+    for (const item of this.tableProducts) {
+      totalPrice += this.getPriceTimesQuantity(item);
+    }
+    return totalPrice;
+  }
 }
