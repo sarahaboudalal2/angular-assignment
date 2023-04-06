1d6d1bb3a0648908599e588804f3dd7a2a24da07
sarahaboudalal <sarahaboudalal@gmail.com>
2023-04-07 01:06:09 +0300

    applied table styling using primeNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { HttpClientModule } from '@angular/common/http';
 import { TableComponent } from './table/table.component';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [AppComponent, ProductsComponent, TableComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, TableModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
